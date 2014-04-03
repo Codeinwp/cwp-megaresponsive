2876dca7358c9c96ee550d58c8047130b808c03b
rodica-andronache <rodik_andronache@yahoo.com>
2014-04-03 13:51:58 +0300

    Refacere incarcare posturi prin load , in pagina de arhiva si sa nu se mai multiplice

--- inc/customizer.php
+++ inc/customizer.php
@@ -1,56 +1,56 @@
-<?php
-/**
- * CWP-MegaR Theme Customizer
- *
- * @package CWP-MegaR
- */
-
-/**
- * Add postMessage support for site title and description for the Theme Customizer.
- *
- * @param WP_Customize_Manager $wp_customize Theme Customizer object.
- */
-function cwp_megar_customize_register( $wp_customize ) {
-	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
-	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
-	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
-    
-$wp_customize->add_section( 'themeslug_logo_section' , array(
-    'title'       => __( 'Logo', 'themeslug' ),
-    'priority'    => 30,
-    'description' => 'Upload a logo to replace the default site name and description in the header',
-) );
-
-$wp_customize->add_setting( 'themeslug_logo' );
-
-$wp_customize->add_control( new WP_Customize_Image_Control( $wp_customize, 'themeslug_logo', array(
-    'label'    => __( 'Logo', 'themeslug' ),
-    'section'  => 'themeslug_logo_section',
-    'settings' => 'themeslug_logo',
-) ) );       
-    
-    
-    
-    
-}
-add_action( 'customize_register', 'cwp_megar_customize_register' );
-
-/**
- * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
- */
-function cwp_megar_customize_preview_js() {
-	wp_enqueue_script( 'cwp_megar_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20130508', true );
-}
-add_action( 'customize_preview_init', 'cwp_megar_customize_preview_js' );
-
-
-
-
-
-
-
-
-
-
-
-
+<?php
+/**
+ * CWP-MegaR Theme Customizer
+ *
+ * @package CWP-MegaR
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function cwp_megar_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+    
+$wp_customize->add_section( 'themeslug_logo_section' , array(
+    'title'       => __( 'Logo', 'themeslug' ),
+    'priority'    => 30,
+    'description' => 'Upload a logo to replace the default site name and description in the header',
+) );
+
+$wp_customize->add_setting( 'themeslug_logo' );
+
+$wp_customize->add_control( new WP_Customize_Image_Control( $wp_customize, 'themeslug_logo', array(
+    'label'    => __( 'Logo', 'themeslug' ),
+    'section'  => 'themeslug_logo_section',
+    'settings' => 'themeslug_logo',
+) ) );       
+    
+    
+    
+    
+}
+add_action( 'customize_register', 'cwp_megar_customize_register' );
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function cwp_megar_customize_preview_js() {
+	wp_enqueue_script( 'cwp_megar_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20130508', true );
+}
+add_action( 'customize_preview_init', 'cwp_megar_customize_preview_js' );
+
+
+
+
+
+
+
+
+
+
+
+
--- js/ajaxLoop.js
+++ js/ajaxLoop.js
@@ -12,197 +12,11 @@ jQuery(function($){
 	var authorvar = $("#author_id").val();
 	var tagvar = $("#tag_id").val();
 	
-	if(typeof catid  !== 'undefined') {
-
+	
 		var load_posts = function(){
             $.ajax({
                 type       : "GET",
-                data       : {action: 'cwp_loop',numPosts : 4, pageNumber: page, catNumber: catid},
-                dataType   : "html",
-                url        : ajaxurl,
-                beforeSend : function(data,settings){
-				
-					if ($("#temp_load").length === 0){
-				
-						$content.append('<div id="temp_load" style="text-align:center">\
-                            <img src="' + $('#load_posts').val() + '/images/ajax-loader.gif" />\
-                            </div>');
-					}		
-  				
-                },
-                success    : function(data){
-					page++;
-					$("#temp_load").remove();
-                    $data = $(data);
-		              
-                     if( $data.length != 0 ){ 
-                        $content.append($data); 
-                     }
-                    else { 
-                          load = false;
-                    }
-                },
-                error     : function(jqXHR, textStatus, errorThrown) {
-                    $("#temp_load").remove();
-                  
-                }
-			});
-		}
-	}
-	else if((typeof yearvar  !== 'undefined') && (typeof monthvar  !== 'undefined')) {
-
-		var load_posts = function(){
-            $.ajax({
-                type       : "GET",
-                data       : {action: 'cwp_loop',numPosts : 4, pageNumber: page, yearPar : yearvar, monthPar : monthvar},
-                dataType   : "html",
-                url        : ajaxurl,
-                beforeSend : function(data,settings){
-				
-					if ($("#temp_load").length === 0){
-				
-						$content.append('<div id="temp_load" style="text-align:center">\
-                            <img src="' + $('#load_posts').val() + '/images/ajax-loader.gif" />\
-                            </div>');
-					}		
-  				
-                },
-                success    : function(data){
-					page++;
-					$("#temp_load").remove();
-                    $data = $(data);
-		              
-                     if( $data.length != 0 ){ 
-                        $content.append($data); 
-                     }
-                    else { 
-                          load = false;
-                    }
-                },
-                error     : function(jqXHR, textStatus, errorThrown) {
-                    $("#temp_load").remove();
-                  
-                }
-			});
-		}
-	}
-	else if(typeof yearvar  !== 'undefined') {
-
-		var load_posts = function(){
-            $.ajax({
-                type       : "GET",
-                data       : {action: 'cwp_loop',numPosts : 4, pageNumber: page, yearPar : yearvar},
-                dataType   : "html",
-                url        : ajaxurl,
-                beforeSend : function(data,settings){
-				
-					if ($("#temp_load").length === 0){
-				
-						$content.append('<div id="temp_load" style="text-align:center">\
-                            <img src="' + $('#load_posts').val() + '/images/ajax-loader.gif" />\
-                            </div>');
-					}		
-  				
-                },
-                success    : function(data){
-					page++;
-					$("#temp_load").remove();
-                    $data = $(data);
-		              
-                     if( $data.length != 0 ){ 
-                        $content.append($data); 
-                     }
-                    else { 
-                          load = false;
-                    }
-                },
-                error     : function(jqXHR, textStatus, errorThrown) {
-                    $("#temp_load").remove();
-                  
-                }
-			});
-		}
-	}
-	else if(typeof authorvar  !== 'undefined') {
-
-		var load_posts = function(){
-            $.ajax({
-                type       : "GET",
-                data       : {action: 'cwp_loop',numPosts : 4, pageNumber: page, authorPar : authorvar},
-                dataType   : "html",
-                url        : ajaxurl,
-                beforeSend : function(data,settings){
-				
-					if ($("#temp_load").length === 0){
-				
-						$content.append('<div id="temp_load" style="text-align:center">\
-                            <img src="' + $('#load_posts').val() + '/images/ajax-loader.gif" />\
-                            </div>');
-					}		
-  				
-                },
-                success    : function(data){
-					page++;
-					$("#temp_load").remove();
-                    $data = $(data);
-		              
-                     if( $data.length != 0 ){ 
-                        $content.append($data); 
-                     }
-                    else { 
-                          load = false;
-                    }
-                },
-                error     : function(jqXHR, textStatus, errorThrown) {
-                    $("#temp_load").remove();
-                  
-                }
-			});
-		}
-	}
-	else if(typeof tagvar  !== 'undefined') {
-
-		var load_posts = function(){
-            $.ajax({
-                type       : "GET",
-                data       : {action: 'cwp_loop',numPosts : 4, pageNumber: page, tagPar : tagvar},
-                dataType   : "html",
-                url        : ajaxurl,
-                beforeSend : function(data,settings){
-				
-					if ($("#temp_load").length === 0){
-				
-						$content.append('<div id="temp_load" style="text-align:center">\
-                            <img src="' + $('#load_posts').val() + '/images/ajax-loader.gif" />\
-                            </div>');
-					}		
-  				
-                },
-                success    : function(data){
-					page++;
-					$("#temp_load").remove();
-                    $data = $(data);
-		              
-                     if( $data.length != 0 ){ 
-                        $content.append($data); 
-                     }
-                    else { 
-                          load = false;
-                    }
-                },
-                error     : function(jqXHR, textStatus, errorThrown) {
-                    $("#temp_load").remove();
-                  
-                }
-			});
-		}
-	}
-	else {
-		
-		var load_posts = function(){
-            $.ajax({
-                type       : "GET",
-                data       : {action: 'cwp_loop',numPosts : 4, pageNumber: page},
+                data       : {action: 'cwp_loop',numPosts : 4, pageNumber: page, catNumber: catid, yearPar: yearvar, monthPar: monthvar, authorPar: authorvar, tagPar: tagvar},
                 dataType   : "html",
                 url        : ajaxurl,
                 beforeSend : function(data,settings){
@@ -232,8 +46,7 @@ jQuery(function($){
                     
                 }
 			});
-    }
-	}
+		}
 		
 	
     
@@ -243,8 +56,10 @@ jQuery(function($){
             var content_offset = $content.offset(); 
 //            loading = true;
             
-            if((load == true)&&($(window).scrollTop() >= $(document).height() - $(window).height() - 100))
+            if((load == true)&&($(window).scrollTop() >= $(document).height() - $(window).height() - 100)) {
                 load_posts();
+				page++;
+			}
     })
   
 load_posts();
